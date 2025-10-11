6f385a495cb23dbc84902f084411fbb17998299f
kwadwoafriyie <afriyiekwadwo2001@gmail.com>
2025-10-11 07:12:44 +0000

    newsletter commented for the future

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -335,8 +335,8 @@ export default function Shop() {
         </div>
       </section>
 
-      {/* Newsletter Section */}
-      <section className="py-16 bg-black text-white">
+      {/* Newsletter Section 
+     <section className="py-16 bg-black text-white">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
           <h2 className="text-3xl font-bold mb-4">
             Stay Updated with New Arrivals
@@ -355,7 +355,7 @@ export default function Shop() {
             </button>
           </div>
         </div>
-      </section>
+      </section> */}
 
       {/* Add to Cart Modal */}
       {selectedProduct && (
